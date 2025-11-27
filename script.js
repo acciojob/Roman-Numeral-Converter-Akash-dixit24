4b1baba530163d07d024c9d2f71e2c430c44586d
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2025-11-27 08:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
+
 function convertToRoman(num) {
   num = Number(num);
 
-  if (num === 0) return "";
+  if (num === 0) return "";  // Roman numerals have no zero
 
   const symbols = [
     ["M", 1000],
-    ["CM", 900],
     ["D", 500],
-    ["CD", 400],
     ["C", 100],
-    ["XC", 90],
     ["L", 50],
-    ["XL", 40],
     ["X", 10],
-    ["IX", 9],
     ["V", 5],
-    ["IV", 4],
     ["I", 1]
   ];
 
@@ -31,6 +26,5 @@ function convertToRoman(num) {
   return result;
 }
 
-
-
+// Example usage:
 alert(convertToRoman(number));
