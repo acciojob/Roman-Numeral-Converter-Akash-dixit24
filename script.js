b6807c63b960bea21a199e4c5442ff29d0ebdb54
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2026-06-04 09:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 function convertToRoman(num) {
-  num = Number(num);
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
-  if (num === 0) return "";
+    let result = '';
 
-  const symbols = [
-    ["M", 1000],
-    ["D", 500],
-    ["C", 100],
-    ["L", 50],
-    ["X", 10],
-    ["V", 5],
-    ["I", 1]
-  ];
-
-  let result = "";
-
-  for (let [roman, value] of symbols) {
-    while (num >= value) {
-      result += roman;
-      num -= value;
+    for (let [symbol, value] of romanNumerals) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
     }
-  }
 
-  return result;
+    return result;
 }
