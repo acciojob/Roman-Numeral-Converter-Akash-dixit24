599ec56a924bd6c40e014f980be839eef87d9d13
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2026-06-04 11:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function convertToRoman(num) {
-    const romanNumerals = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
+function convertToRomanLarge(num) {
+  if (num === 0) return "N";
+  if (num < 0 || num > 100000) return "Out of range (0-100000)";
 
-    let roman = "";
+  const romanMatrix = [
+    [100000, 'C_'], [90000, 'XC_'], [50000, 'L_'], [40000, 'XL_'], [10000, 'X_'],
+    [9000, 'MX_'], [5000, 'V_'], [4000, 'MV_'], [1000, 'M'], [900, 'CM'],
+    [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'],
+    [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+  ];
 
-    for (let i = 0; i < romanNumerals.length; i++) {
-        while (num >= romanNumerals[i][1]) {
-            roman += romanNumerals[i][0];
-            num -= romanNumerals[i][1];
-        }
+  let result = '';
+  for (let i = 0; i < romanMatrix.length; i++) {
+    const [value, numeral] = romanMatrix[i];
+    while (num >= value) {
+      result += numeral;
+      num -= value;
     }
-
-    return roman;
+  }
+  return result;
 }
 
-window.convertToRoman = convertToRoman;
+// Function to handle the user interface action
+function handleConversion() {
+  const inputVal = document.getElementById("userInput").value;
+  
+  // Convert text string input into an integer number base 10
+  const parsedNum = parseInt(inputVal, 10);
+
+  if (isNaN(parsedNum)) {
+    document.getElementById("result").innerText = "Please enter a valid number.";
+    return;
+  }
+
+  const romanResult = convertToRomanLarge(parsedNum);
+  document.getElementById("result").innerText = romanResult;
+}
